3304b7d048d3436727a4d0e0c58d9f663396f9c1
HosamElian <hosamemm@gmail.com>
2023-10-07 01:09:20 +0300

    Modify UI

--- ProductCatalog.Models/Entity/Product.cs
+++ ProductCatalog.Models/Entity/Product.cs
@@ -17,7 +17,9 @@ namespace ProductCatalog.Models.Entity
         [Required]
         [Range(1, 1000_000)]
         public double Price { get; set; }
-        public int Quantity { get; set; }
+		[Range(1, 1000_000)]
+
+		public int Quantity { get; set; }
         [Required]
         [MaxLength(100)]
         public string Name { get; set; }
